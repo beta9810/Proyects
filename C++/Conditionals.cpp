531bd0709bcd3f44db9aa5d582a4698a768d3ce3
Josesito <jdbenitez896@misena.edu.co>
2023-04-03 14:56:51 -0500

    Conditionals V2 switch

--- C++/Conditionals.cpp
+++ C++/Conditionals.cpp
@@ -20,12 +20,41 @@ int main(){
 	
 	cout<<"Please insert a number: ";cin>>number;
 	
+/* 	This is for if i know the conditional is the same of de value
 	if(number == data){
     cout<<"The number is 5";
 }
  else{
     cout<<"The number is diferent to 5";
 }
+*/
 
+/*This is for if i know the conditional is diferent of de value
+if(number != data){
+    cout<<"The number is 5";
+}
+ else{
+    cout<<"The number is diferent to 5";
+}
+*/
+
+
+/* And this is if i want know that value is more o less that value
+if(number <=> data){
+    cout<<"The number is 5";
+}
+ else{
+    cout<<"The number is diferent to 5";
+}
+*/
 	return 0;
 }
+
+
+
+
+
+
+
+
+
