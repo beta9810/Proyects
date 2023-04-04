527355ab71eed14edaf674cd18d6c34ea48a6cbf
Josesito <jdbenitez896@misena.edu.co>
2023-04-04 16:47:00 -0500

    Exercise N9 of Conditionals

---
+++ C++/ConditionalsExerciseN9.cpp
@@ -0,0 +1,23 @@
+//Conditionals Exercise N9: Cambiar un número entero con el mismo valor pero en romanos.
+
+/*
+M = 1000
+D = 500
+C = 100
+L = 50
+X = 10
+V = 5
+I=1
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	
+	
+	
+	return 0;
+}
