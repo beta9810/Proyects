5081f4564504b711b9135b2c2a4c77e106c509e5
Josesito <jdbenitez896@misena.edu.co>
2023-05-15 17:18:44 -0500

    Exercise 5

---
+++ C++/ArrangementExerciseN5.cpp
@@ -0,0 +1,29 @@
+//Arrangement Exercise N° 5: Desarrolle un programa que lea de la entrada
+//estándar un vector de enteros y determine el mayor elemento del vector.
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main(){
+	int numbers [100],n, mayor=0;
+	
+	cout<<"Please insert amount of numbers for this Array"; cin>>n;
+	
+	for (int i=0;i<n;i++){
+		cout<<i+1<<". Insert a number: ";//Digite un número:
+		cin>>numbers[i];
+		
+		if(numbers[i] > mayor){
+			mayor = numbers[i];
+		}
+	}
+	
+	cout<<"\nThe number up of vector is: "<<mayor<<endl;
+	
+	
+	getch();
+	return 0;
+}
+
