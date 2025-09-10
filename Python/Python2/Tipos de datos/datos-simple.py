f8cfa2e44cdb9b01fecb40be394c52db9b57c44c
Jose Conexo <josebenitez9810@gmail.com>
2025-09-10 15:35:16 -0500

    10-09-2025

---
+++ Python/Python2/Tipos de datos/datos-simple.py
@@ -0,0 +1,17 @@
+"string"
+'string'
+
+"""tus datos son:
+     nombre: Jose
+     apellido: Hermoso"""
+     
+'''tus datos son:
+     nombre: Jose
+     apellido: Hermoso'''
+     
+    
+40
+40.2
+
+True
+False
