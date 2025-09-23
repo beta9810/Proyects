7dc70f7f0e8d9613084f9faef4b0bab8b53b7bf8
Beta <josebenitez9810@gmail.com>
2025-09-22 21:54:51 -0500

    22/09/2025

---
+++ Python/Python2/condicionales/else-if.py
@@ -0,0 +1,25 @@
+ingreso_mensual = 92000
+gasto_mensual = 80000
+
+
+#if anidados y else if (elif)
+
+if ingreso_mensual > 100000:
+    if ingreso_mensual - gasto_mensual < 0:
+        print ("Estás quedado")
+    elif ingreso_mensual - gasto_mensual > 3000:
+        print ("Estás melo")
+    else:
+        print ("Jmm papi pilas")
+    
+elif ingreso_mensual > 1000:
+    print ("Estás bien en latinoamerica")
+    
+elif ingreso_mensual > 1000:
+    print ("Estás bien en latinoamerica")
+    
+elif ingreso_mensual > 1000:
+    print ("Estás bien en latinoamerica")
+
+else:
+    print("Eres pobre")
