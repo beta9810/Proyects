41fcf275c4e99a9d065f018e5313826f08bebfce
Beta <josebenitez9810@gmail.com>
2025-09-23 21:07:03 -0500

    23/09/2025

---
+++ Python/Python2/operadores/op_logicos.py
@@ -0,0 +1,21 @@
+#AND
+
+Resultado = True & True #Devolver True
+Resultado2 = False & True #Devolver Falso
+Resultado3 = True & False #Devolver Falso
+Resultado4 = False & False #Devolver Falso
+
+#OR
+
+Resultado5 = True | True #Devolver True
+Resultado6 = False | True #Devolver True
+Resultado7 = True | False #Devolver True
+Resultado8 = False | False #Devolver Falso
+
+#NOT
+
+Resultado = not True #Devolver Falso
+Resultado = not False #Devolver True
+
+
+#Las funciones analisan pedazos de código que le damos para devolvernos o hacer algo especificas con ellos
